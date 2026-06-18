ce8655ce2cf3f2e05f996a7cd26ff896d3aad9d7
Augusto Andrei de Melo Maux <20241041110009@ifrn.local>
2026-06-18 07:55:53 -0300

    Adicionando arquivos das rotas

--- app/routes.py
+++ app/routes.py
@@ -2,5 +2,23 @@ from flask import render_template
 from . import bp
 
 @bp.route("/")
-def login():
-    render_template('index.html')
+def index():
+    return render_template('index.html')
+
+@bp.route("/avistamentos")
+def avistamentos():
+    aves = [
+        "Arara",
+        "Calidris Alba",
+        "Maçarico Branco"
+    ]
+
+    return render_template('avistamentos.html', aves=aves)
+
+@bp.route("/sobre")
+def sobre():
+    return render_template('sobre.html')
+
+@bp.route("/mapa")
+def mapa():
+    return render_template('mapa.html')
